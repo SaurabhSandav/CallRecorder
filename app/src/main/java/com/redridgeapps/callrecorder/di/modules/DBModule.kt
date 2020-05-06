91d25081585b5189c70a27f0ee7996b83d523a4a
Saurabh Sandav <justmailsaurabh@gmail.com>
2020-05-06 11:07:10 +0530

    SqlDelight: Use EnumColumnAdapter from library
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/callrecorder/di/modules/DBModule.kt
+++ app/src/main/java/com/redridgeapps/callrecorder/di/modules/DBModule.kt
@@ -4,10 +4,8 @@ import android.content.Context
 import com.redridgeapps.callrecorder.CallRecordingDB
 import com.redridgeapps.callrecorder.Recording
 import com.redridgeapps.callrecorder.RecordingQueries
-import com.redridgeapps.callrecorder.callutils.CallDirection
-import com.redridgeapps.callrecorder.db.EnumTextColumnAdapter
 import com.redridgeapps.callrecorder.db.InstantIntegerColumnAdapter
-import com.squareup.sqldelight.ColumnAdapter
+import com.squareup.sqldelight.EnumColumnAdapter
 import com.squareup.sqldelight.android.AndroidSqliteDriver
 import dagger.Module
 import dagger.Provides
@@ -16,7 +14,6 @@ import javax.inject.Singleton
 @Module
 object DBModule {
 
-    @Suppress("UNCHECKED_CAST")
     @Provides
     @Singleton
     fun Context.provideCallRecordingDB(): CallRecordingDB {
@@ -29,7 +26,7 @@ object DBModule {
             RecordingAdapter = Recording.Adapter(
                 start_instantAdapter = InstantIntegerColumnAdapter,
                 end_instantAdapter = InstantIntegerColumnAdapter,
-                call_directionAdapter = EnumTextColumnAdapter(CallDirection::valueOf) as ColumnAdapter<CallDirection, String>
+                call_directionAdapter = EnumColumnAdapter()
             )
         )
     }
