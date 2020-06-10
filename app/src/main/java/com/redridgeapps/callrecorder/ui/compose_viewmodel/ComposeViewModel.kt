71a54e7027a3560c4f7fbf18969c30d00f6a4b89
Saurabh Sandav <justmailsaurabh@gmail.com>
2020-06-10 15:57:33 +0530

    Rename viewModelStoreKey to viewModelKey, Make private
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/callrecorder/ui/compose_viewmodel/ComposeViewModel.kt
+++ app/src/main/java/com/redridgeapps/callrecorder/ui/compose_viewmodel/ComposeViewModel.kt
@@ -6,23 +6,27 @@ import com.koduok.compose.navigation.BackStackAmbient
 import com.koduok.compose.navigation.core.Route
 import com.koduok.compose.navigation.core.backStackController
 import com.redridgeapps.callrecorder.ui.routing.RouterBackStackListener
+import kotlin.reflect.KClass
 
 @Composable
-inline fun <reified T : ViewModel> fetchViewModel(): T {
+inline fun <reified T : ViewModel> fetchViewModel(): T = fetchViewModel(T::class)
+
+@Composable
+fun <T : ViewModel> fetchViewModel(kClass: KClass<T>): T {
     val viewModelFetcher = ViewModelFetcherAmbient.current
-    val key = BackStackAmbient.current.current.viewModelStoreKey
-    return viewModelFetcher.fetch(key, T::class)
+    val key = BackStackAmbient.current.current.viewModelKey
+    return viewModelFetcher.fetch(key, kClass)
 }
 
 fun ComposeFramework.setupViewModel() {
 
     val backStackListener = RouterBackStackListener(
-        onRouteAdded = { initializeViewModel(it.viewModelStoreKey) },
-        onRouteRemoved = { destroyViewModel(it.viewModelStoreKey) }
+        onRouteAdded = { initializeViewModel(it.viewModelKey) },
+        onRouteRemoved = { destroyViewModel(it.viewModelKey) }
     )
 
     backStackController.addListener(backStackListener)
 }
 
-val Route<*>.viewModelStoreKey: String
+private val Route<*>.viewModelKey: String
     get() = "${key}_$index"
