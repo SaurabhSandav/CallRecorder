b54f0bf39a6840f5ea121edc4cd00b85da601cfe
Saurabh Sandav <justmailsaurabh@gmail.com>
2020-06-12 19:07:05 +0530

    Move PCM enum and extensions into PCM file
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/callrecorder/callutils/PCM.kt
+++ app/src/main/java/com/redridgeapps/callrecorder/callutils/PCM.kt
@@ -1,5 +1,6 @@
 package com.redridgeapps.callrecorder.callutils
 
+import android.media.AudioFormat
 import com.redridgeapps.wavutils.WavBitsPerSample
 import com.redridgeapps.wavutils.WavChannels
 import com.redridgeapps.wavutils.WavSampleRate
@@ -22,6 +23,17 @@ enum class PcmEncoding(val bitsPerSample: Int) {
     PCM_8BIT(8),
     PCM_16BIT(16),
     PCM_FLOAT(32);
+}
+
+fun PcmChannels.toAudioRecordChannel(): Int = when (this) {
+    PcmChannels.MONO -> AudioFormat.CHANNEL_IN_MONO
+    PcmChannels.STEREO -> AudioFormat.CHANNEL_IN_STEREO
+}
+
+fun PcmEncoding.toAudioRecordEncoding(): Int = when (this) {
+    PcmEncoding.PCM_8BIT -> AudioFormat.ENCODING_PCM_8BIT
+    PcmEncoding.PCM_16BIT -> AudioFormat.ENCODING_PCM_16BIT
+    PcmEncoding.PCM_FLOAT -> AudioFormat.ENCODING_PCM_FLOAT
 }
 
 fun PcmSampleRate.asWavSampleRate(): WavSampleRate = WavSampleRate(sampleRate)
