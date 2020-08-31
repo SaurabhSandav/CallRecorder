06311f2542372eaa9bf306dfb5f986473f267cf3
Saurabh Sandav <justmailsaurabh@gmail.com>
2020-08-31 20:42:01 +0530

    Rewrite FirstRunScreen
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- ui_first_run/src/main/java/com/redridgeapps/ui/firstrun/FirstRunViewModel.kt
+++ ui_first_run/src/main/java/com/redridgeapps/ui/firstrun/FirstRunViewModel.kt
@@ -1,29 +1,70 @@
 package com.redridgeapps.ui.firstrun
 
+import android.Manifest
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.redridgeapps.callrecorder.common.PermissionChecker
 import com.redridgeapps.callrecorder.common.Systemizer
 import com.redridgeapps.callrecorder.common.utils.launchUnit
 import com.redridgeapps.callrecorder.prefs.PREF_IS_FIRST_RUN
 import com.redridgeapps.callrecorder.prefs.PREF_RECORDING_ENABLED
 import com.redridgeapps.callrecorder.prefs.Prefs
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 internal class FirstRunViewModel @ViewModelInject constructor(
     private val systemizer: Systemizer,
-    private val prefs: Prefs
+    private val prefs: Prefs,
+    permissionChecker: PermissionChecker,
 ) : ViewModel() {
 
-    val uiState = FirstRunState(systemizer.isAppSystemizedFlow)
+    private val initialState = FirstRunState(
+        onAppSystemize = this::onAppSystemize,
+        allPermissionsGranted = REQUESTED_DANGEROUS_PERMISSIONS.all {
+            permissionChecker.isPermissionGranted(it)
+        },
+        onPermissionsResult = this::onPermissionResult,
+        captureAudioOutputPermissionGranted = permissionChecker
+            .isPermissionGranted(Manifest.permission.CAPTURE_AUDIO_OUTPUT),
+    )
+    private val _uiState = MutableStateFlow(initialState)
 
-    fun systemize() = viewModelScope.launchUnit {
+    val uiState: StateFlow<FirstRunState> by ::_uiState
+
+    init {
+
+        systemizer.isAppSystemizedFlow
+            .onEach { _uiState.value = _uiState.value.copy(isAppSystemized = it) }
+            .launchIn(viewModelScope)
+
+        uiState.onEach { checkConfigurationFinished(it) }.launchIn(viewModelScope)
+    }
+
+    private fun onAppSystemize() = viewModelScope.launchUnit {
         systemizer.systemize()
     }
 
-    fun configurationFinished() = viewModelScope.launchUnit {
-        prefs.editor {
-            set(PREF_IS_FIRST_RUN, false)
-            set(PREF_RECORDING_ENABLED, true)
+    private fun onPermissionResult(allPermissionsGranted: Boolean) {
+        _uiState.value = _uiState.value.copy(allPermissionsGranted = allPermissionsGranted)
+    }
+
+    private fun checkConfigurationFinished(uiState: FirstRunState) {
+
+        val isConfigFinished = uiState.isAppSystemized
+                && uiState.allPermissionsGranted
+                && uiState.captureAudioOutputPermissionGranted
+
+        if (isConfigFinished) {
+
+            prefs.editor {
+                set(PREF_IS_FIRST_RUN, false)
+                set(PREF_RECORDING_ENABLED, true)
+            }
+
+            _uiState.value = _uiState.value.copy(isConfigFinished = true)
         }
     }
 }
---
+++ ui_first_run/src/main/java/com/redridgeapps/ui/firstrun/ui/SystemizationConfig.kt
@@ -0,0 +1,52 @@
+package com.redridgeapps.ui.firstrun.ui
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import com.redridgeapps.ui.firstrun.OnAppSystemize
+
+@Composable
+internal fun SystemizationConfig(
+    isAppSystemized: Boolean,
+    onAppSystemize: OnAppSystemize,
+) {
+
+    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+
+        Text(text = "Systemization", style = MaterialTheme.typography.h6)
+
+        Text(
+            text = "App needs to be a system app. High quality call recording only works with system apps.",
+            style = MaterialTheme.typography.subtitle1
+        )
+
+        var isAppSystemizedNullable: Boolean? by remember(isAppSystemized) {
+            mutableStateOf(isAppSystemized)
+        }
+
+        Crossfade(current = isAppSystemizedNullable) { isAppSystemizedNullableCF ->
+
+            when (isAppSystemizedNullableCF) {
+                null -> CircularProgressIndicator()
+                true -> Text(text = "✔ App is systemized")
+                false -> Button(
+                    onClick = {
+                        onAppSystemize()
+                        isAppSystemizedNullable = null
+                    },
+                    content = { Text(text = "Systemize App") },
+                )
+            }
+        }
+    }
+}
