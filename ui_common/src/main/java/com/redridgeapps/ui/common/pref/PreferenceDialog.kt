46122a7cac3f72e9ccae9a31127e4292aa932e1d
Saurabh Sandav <justmailsaurabh@gmail.com>
2020-09-04 22:10:02 +0530

    Add MaterialDialog
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- ui_common/src/main/java/com/redridgeapps/ui/common/pref/PreferenceDialog.kt
+++ ui_common/src/main/java/com/redridgeapps/ui/common/pref/PreferenceDialog.kt
@@ -1,19 +1,17 @@
 package com.redridgeapps.ui.common.pref
 
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
+import com.redridgeapps.ui.common.generic.MaterialDialog
 
 @Composable
 internal fun PreferenceDialog(
@@ -23,25 +21,19 @@ internal fun PreferenceDialog(
     content: @Composable ColumnScope.() -> Unit,
 ) {
 
-    Dialog(onDismissRequest = onDismiss) {
+    MaterialDialog(onDismissRequest = onDismiss, modifier = Modifier.width(280.dp)) {
 
-        Surface {
+        // Title
+        Text(
+            text = title,
+            modifier = Modifier.height(56.dp)
+                .padding(horizontal = 24.dp)
+                .wrapContentHeight(Alignment.CenterVertically),
+            style = MaterialTheme.typography.h6
+        )
 
-            Column(Modifier.width(280.dp)) {
+        content()
 
-                // Title
-                Text(
-                    text = title,
-                    modifier = Modifier.height(56.dp)
-                        .padding(horizontal = 24.dp)
-                        .wrapContentHeight(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.h6
-                )
-
-                content()
-
-                buttonContent?.let { it() }
-            }
-        }
+        buttonContent?.let { it() }
     }
 }
